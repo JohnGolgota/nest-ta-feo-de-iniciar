c8393e99c579247a5a0407943e6727740e3c5064
JohnGolgota <js684new@gmail.com>
2023-12-25 14:53:04 -0500

    Si tiene sentido. pero sigue siendo una M

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,23 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
 import { TaskStatus } from "../tasks.entity";
 
 export class createTaskDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
     title: string;
+    @IsString()
     description: string;
 }
 
 export class patchTaskDto {
+    @IsString()
+    @IsOptional()
     title?: string;
+    @IsString()
+    @IsOptional()
     description?: string;
+    @IsEnum(TaskStatus)
+    @IsOptional()
     status?: TaskStatus;
 }
